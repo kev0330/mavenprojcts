d84d39a2fbb87252a1b403a562dfdfaf6b2dd1ea
kev0330 <33414005+kev0330@users.noreply.github.com>
2019-02-13 22:18:11 +0800

    Update App.java

--- src/main/java/cput/ac/za/App.java
+++ src/main/java/cput/ac/za/App.java
@@ -1,15 +1,18 @@
 package cput.ac.za;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {	
+        System.out.println( "Creating Project" );
         System.out.println( "Made a change by Artemis1234");
 	System.out.println( "Made a change by MatthewMarkBrown");
+	App app = new App();
+        System.out.print("num1 + num2 = " + app.calculate(3, 4));
+    }
+	
+	public int calculate(int num1 , int num2){
+        int total = num1 + num2;
+        return total;
     }
 }
