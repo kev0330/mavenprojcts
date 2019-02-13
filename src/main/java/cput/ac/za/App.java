4f9eb5c59d532326c8edc8e2af689320edda0113
iammatthewmb <matrix.brown@gmail.com>
2019-02-13 15:06:42 +0200

    improved mavenProject

--- src/main/java/cput/ac/za/App.java
+++ src/main/java/cput/ac/za/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Made a change by Artemis1234")
+        System.out.println( "Made a change by Artemis1234");
+	System.out.println( "Made a change by MatthewMarkBrown");
     }
 }
